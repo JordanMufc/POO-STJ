ace2be856fcefca6759fef8c7b776d225338a554
PounsyBaby <160937665+PounsyBaby@users.noreply.github.com>
2024-05-27 13:32:41 +0200

    IMP : ajout du personnage, des mouvements et de la caméra

--- Code/entities.py
+++ Code/entities.py
@@ -3,6 +3,28 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.Surface((100,100))
+        self.image = pygame.Surface((16,16))
         self.image.fill('red')
         self.rect = self.image.get_frect(center = pos)
+
+        self.direction = vector()
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        input_vector = vector()
+        if keys[pygame.K_z]:
+            input_vector.y -=1
+        if keys[pygame.K_s]:
+            input_vector.y +=1
+        if keys[pygame.K_q]:
+            input_vector.x -=1
+        if keys[pygame.K_d]:
+            input_vector.x +=1
+        self.direction = input_vector
+
+    def move(self, dt):
+        self.rect.center += self.direction * 125 * dt
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
--- Code/main.py
+++ Code/main.py
@@ -5,15 +5,18 @@ from os.path import join
 
 from sprites import Sprite
 from entities import Player
+from groups import AllSprites
+
 
 class Game: 
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDHT, WINDOW_HEIGHT))
         pygame.display.set_caption('Le donjon de Barleroi')
+        self.clock = pygame.time.Clock()
 
         # Create Group or Groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
 
         self.import_assets()
         self.setup(self.tmx_maps['world'], 'ground')
@@ -26,13 +29,14 @@ class Game:
          for x,y, surf in tmx_map.get_layer_by_name('ground').tiles():
             Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, self.all_sprites)
 
-         for obj in tmx_map.get_layer_by_name('ground'):
+         for obj in tmx_map.get_layer_by_name('entities'):
              if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
-                 Player((obj.x, obj.y), self.all_sprites)
+                 self.player = Player((obj.x, obj.y), self.all_sprites)
             
 
     def run(self):
         while True:
+            dt = self.clock.tick(60) / 1000
             # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -40,7 +44,9 @@ class Game:
                     exit()
 
             # game logic
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.update(dt)
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
 if __name__ == '__main__':
