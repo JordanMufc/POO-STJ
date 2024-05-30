c9c59cc5426657e945ea414c766e6e897783237c
Renard S <renardstaifane@gmail.com>
2024-05-30 15:27:09 +0200

    IMP: animated sprites function
    add animatedsprited function for water, candle, etc

--- Code/main.py
+++ Code/main.py
@@ -27,7 +27,7 @@ class Game:
         self.tmx_maps = {'world': load_pygame(join('..','Data/Maps/World.tmx'))}
 
         self.overworld_frames = {
-            'characters' : all_character_import('..', 'Graphics', 'haracters')
+            'characters' : all_character_import('..', 'Graphics', 'characters')
         }
 
     def setup(self, tmx_map, player_start_pos):
--- Code/sprites.py
+++ Code/sprites.py
@@ -5,3 +5,13 @@ class Sprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+
+class AnimatedSprite(Sprite):
+    def __init__(self, pos, surf, frames, groups):
+        self.frame_index, self.frames = 0 , frames
+        super().__init__(pos, surf, frames[self.frame_index], groups)
+    def animate(self, dt):
+        self.famre_index += 4 * dt
+        self.image = self.frames[int(self.frame_index %len(self.frames))]
+    def update(self, dt):
+        self.animate(dt)
