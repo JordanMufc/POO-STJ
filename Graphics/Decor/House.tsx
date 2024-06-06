78524edc0db3cc3f046034f179c8035875eaa7d8
JordanMufc <jordanpaquet27@icloud.com>
2024-06-06 17:39:20 +0200

    del supprimer mauvais projet...

--- Graphics/Decor/House.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="House" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="../../assets/maps/pack/Houses_Pack 32x32/houses.png" width="1024" height="1024"/>
-</tileset>
